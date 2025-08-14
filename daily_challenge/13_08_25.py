0302fb8f9749e0549ede141c15ba8fd315aa1756
Joel <reyesvargasjoel@gmail.com>
2025-08-13 22:53:02 -0600

    feat: daily problem solved

--- daily_challenge/13_08_25.py
+++ daily_challenge/13_08_25.py
@@ -7,3 +7,35 @@ Note:
 A substring is a contiguous sequence of characters within a string.
 There may be leading zeroes in num or a good integer.
 """
+
+class Solution(object):
+    def largestGoodInteger(self, num):
+        """
+        :type num: str
+        :rtype: str
+        """
+        max_good = ""
+        for i in range(len(num) - 2):
+            if num[i] == num[i + 1] == num[i + 2]:
+                good_int = num[i:i + 3]
+                if good_int > max_good:
+                    max_good = good_int
+        return max_good
+
+# Unit tests
+import unittest
+
+class TestLargestGoodInteger(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_largest_good_integer(self):
+        self.assertEqual(self.solution.largestGoodInteger("6777133339"), "777")
+        self.assertEqual(self.solution.largestGoodInteger("2300019"), "000")
+        self.assertEqual(self.solution.largestGoodInteger("42352338"), "")
+        self.assertEqual(self.solution.largestGoodInteger("999"), "999")
+        self.assertEqual(self.solution.largestGoodInteger("1234567890"), "")
+        self.assertEqual(self.solution.largestGoodInteger("111222333444555666777888999000"), "999")
+
+if __name__ == "__main__":
+    unittest.main()
