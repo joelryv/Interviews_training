92ad76ca72d3a607d4339f9dee3cb738cc249139
Joel <reyesvargasjoel@gmail.com>
2025-08-14 00:04:30 -0600

    feat: unit test added

--- 01_array/08_move_zeroes.py
+++ 01_array/08_move_zeroes.py
@@ -15,3 +15,29 @@ class Solution(object):
                 nums[j],nums[pos]=nums[pos],nums[j]
                 pos+=1
         return pos
+    
+# Unit tests
+import unittest
+class TestMoveZeroes(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_move_zeroes(self):
+        nums1 = [0, 1, 0, 3, 12]
+        self.solution.moveZeroes(nums1)
+        self.assertEqual(nums1, [1, 3, 12, 0, 0])
+
+        nums2 = [0]
+        self.solution.moveZeroes(nums2)
+        self.assertEqual(nums2, [0])
+
+        nums3 = [1, 2, 3]
+        self.solution.moveZeroes(nums3)
+        self.assertEqual(nums3, [1, 2, 3])
+
+        nums4 = [0, 0, 1]
+        self.solution.moveZeroes(nums4)
+        self.assertEqual(nums4, [1, 0, 0])
+
+if __name__ == "__main__":
+    unittest.main()
