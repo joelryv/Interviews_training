fa83cc2d55b7ea426b3a39fece65e2bc8dbdbe7d
Joel <reyesvargasjoel@gmail.com>
2025-08-15 23:42:47 -0600

    feat: problem solved

--- 01_array/10_valid_sudoku.py
+++ 01_array/10_valid_sudoku.py
@@ -7,3 +7,80 @@ Note:
 A Sudoku board (partially filled) could be valid but is not necessarily solvable.
 Only the filled cells need to be validated according to the mentioned rules.
 """
+from typing import List
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for row in board:
+            seen = ''
+            for n in row:
+                if n in seen:
+                    return False
+                if n != '.':
+                    seen += n
+        for i in range(9):
+            seen = ''
+            for j in range(9):
+                n = board[j][i]
+                if n in seen:
+                    return False
+                if n != '.':
+                    seen += n
+        for a in range(3):
+            for b in range(3):
+                seen = ''
+                for i in range(3):
+                    for j in range(3):
+                        n = board[(3*a)+i][(3*b)+j]
+                        if n in seen:
+                            return False
+                        if n != '.':
+                            seen += n
+        return True
+    
+# Unit tests
+import unittest
+class TestIsValidSudoku(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_isValidSudoku(self):
+        board1 = [
+            ["5","3",".",".","7",".",".",".","."],
+            ["6",".",".","1","9","5",".",".","."],
+            [".","9","8",".",".",".",".","6","."],
+            ["8",".",".",".","6",".",".",".","3"],
+            ["4",".",".","8",".","3",".",".","1"],
+            ["7",".",".",".","2",".",".",".","6"],
+            [".","6",".",".",".",".","2","8","."],
+            [".",".",".","4","1","9",".",".","5"],
+            [".",".",".",".","8",".",".","7","9"]
+        ]
+        board2 = [
+            ["8","3",".",".","7",".",".",".","."],
+            ["6",".",".","1","9","5",".",".","."],
+            [".","9","8",".",".",".",".","6","."],
+            ["8",".",".",".","6",".",".",".","3"],
+            ["4",".",".","8",".","3",".",".","1"],
+            ["7",".",".",".","2",".",".",".","6"],
+            [".","6",".",".",".",".","2","8","."],
+            [".",".",".","4","1","9",".",".","5"],
+            [".",".",".",".","8",".",".","7","9"]
+        ]
+        board3 = [
+            [".",".","4",".",".",".","6","3","."],
+            [".",".",".",".",".",".",".",".","."],
+            ["5",".",".",".",".",".",".","9","."],
+            [".",".",".","5","6",".",".",".","."],
+            ["4",".","3",".",".",".",".",".","1"],
+            [".",".",".","7",".",".",".",".","."],
+            [".",".",".","5",".",".",".",".","."],
+            [".",".",".",".",".",".",".",".","."],
+            [".",".",".",".",".",".",".",".","."]
+        ]
+
+        self.assertTrue(self.solution.isValidSudoku(board1))
+        self.assertFalse(self.solution.isValidSudoku(board2))
+
+if __name__ == "__main__":
+    unittest.main()
