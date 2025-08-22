6c8f3b07a3b93bff1d227dcc704e6bd7cd26cfcd
Joel <reyesvargasjoel@gmail.com>
2025-08-22 00:37:05 -0600

    feat: problem solved

--- 02_string/02_reverse_integer.py
+++ 02_string/02_reverse_integer.py
@@ -1,3 +1,11 @@
 """
 https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/880/
 """
+class Solution:
+    def reverse(self, x: int) -> int:
+        n=0
+        if x<0:
+            n=-int(str(-x)[::-1]) 
+        else:
+            n= int(str(x)[::-1])
+        return n if n.bit_length()<32 else 0
