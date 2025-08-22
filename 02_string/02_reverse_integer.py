254e4cf0f0142fe064fef819a079356251a15307
Joel <reyesvargasjoel@gmail.com>
2025-08-22 00:38:20 -0600

    feat: unit test added

--- 02_string/02_reverse_integer.py
+++ 02_string/02_reverse_integer.py
@@ -9,3 +9,17 @@ class Solution:
         else:
             n= int(str(x)[::-1])
         return n if n.bit_length()<32 else 0
+
+# Unit tests    
+import unittest
+class TestSolution(unittest.TestCase):
+    def test_reverse(self):
+        solution = Solution()
+        self.assertEqual(solution.reverse(123), 321)
+        self.assertEqual(solution.reverse(-123), -321)
+        self.assertEqual(solution.reverse(120), 21)
+        self.assertEqual(solution.reverse(0), 0)
+        self.assertEqual(solution.reverse(1534236469), 0)
+
+if __name__ == "__main__":
+    unittest.main()
