391b88b37eba651aad4108906365e1d3614726a3
Joel <reyesvargasjoel@gmail.com>
2025-08-23 13:51:40 -0600

    feat: problem solved

--- 02_string/04_valid_anagram.py
+++ 02_string/04_valid_anagram.py
@@ -1,3 +1,14 @@
 """
 https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/882/
 """
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d = {}
+        for c in s:
+            d[c] = d.get(c, 0) + 1
+        for c in t:
+            d[c] = d.get(c, -1) - 1
+        for key, value in d.items():
+            if value != 0:
+                return False
+        return True
