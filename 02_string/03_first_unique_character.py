1eb83aa1a266b50257c801d56757bdc74de02e9a
Joel <reyesvargasjoel@gmail.com>
2025-08-23 12:20:09 -0600

    feat: unit tests added

--- 02_string/03_first_unique_character.py
+++ 02_string/03_first_unique_character.py
@@ -10,3 +10,49 @@ class Solution:
             if count[char] == 1:
                 return i
         return -1
+    
+# Unittests
+import unittest 
+class TestFirstUniqueCharacter(unittest.TestCase):
+    def test_example1(self):
+        s = "leetcode"
+        expected = 0
+        self.assertEqual(Solution().firstUniqChar(s), expected)
+
+    def test_example2(self):
+        s = "loveleetcode"
+        expected = 2
+        self.assertEqual(Solution().firstUniqChar(s), expected)
+
+    def test_example3(self):
+        s = "aabb"
+        expected = -1
+        self.assertEqual(Solution().firstUniqChar(s), expected)
+
+    def test_empty_string(self):
+        s = ""
+        expected = -1
+        self.assertEqual(Solution().firstUniqChar(s), expected)
+
+    def test_single_character(self):
+        s = "z"
+        expected = 0
+        self.assertEqual(Solution().firstUniqChar(s), expected)
+
+    def test_all_repeating_characters(self):
+        s = "aaabbbccc"
+        expected = -1
+        self.assertEqual(Solution().firstUniqChar(s), expected)
+
+    def test_first_character_unique(self):
+        s = "zxyxzy"
+        expected = -1
+        self.assertEqual(Solution().firstUniqChar(s), expected)
+
+    def test_last_character_unique(self):
+        s = "xxyyzzp"
+        expected = 6
+        self.assertEqual(Solution().firstUniqChar(s), expected)
+
+if __name__ == "__main__":
+    unittest.main()
