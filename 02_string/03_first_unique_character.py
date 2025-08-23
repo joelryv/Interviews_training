8dd1c9ecdd61f27b8eeec372aad490faa91ce21f
Joel <reyesvargasjoel@gmail.com>
2025-08-23 12:19:48 -0600

    feat: problem solved

--- 02_string/03_first_unique_character.py
+++ 02_string/03_first_unique_character.py
@@ -1,3 +1,12 @@
 """
 https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/881/
 """
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = {}
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+        for i, char in enumerate(s):
+            if count[char] == 1:
+                return i
+        return -1
