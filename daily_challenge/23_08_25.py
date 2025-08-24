c31ea07a283b20eb877fa4996860f7a150107f54
Joel <reyesvargasjoel@gmail.com>
2025-08-23 21:31:37 -0600

    feat: problem solved

--- daily_challenge/23_08_25.py
+++ daily_challenge/23_08_25.py
@@ -1,3 +1,23 @@
 """
 https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/?envType=daily-question&envId=2025-08-24
 """
+
+from typing import List
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        zcount = 0
+        longest = 0
+        start = 0
+
+        for i in range(len(nums)):
+            if nums[i]==0:
+                zcount += 1
+            while (zcount > 1):
+                if nums[start]==0:
+                    zcount -= 1
+                start += 1
+            if (i - start) > longest:
+                longest = (i - start)
+        return longest
+    
