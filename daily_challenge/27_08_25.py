e5912a6d8b8104643030a061258efa6dbf2a6ff1
Joel <reyesvargasjoel@gmail.com>
2025-08-27 23:21:26 -0600

    feat: test cases added

--- daily_challenge/27_08_25.py
+++ daily_challenge/27_08_25.py
@@ -19,3 +19,25 @@ class Solution:
                 grid[i][j + i] = tmp[i]
         return grid
     
+# Unittests
+import unittest
+
+class TestSolution(unittest.TestCase):
+    
+    def test_case_1(self):
+        grid = [[1,7,3],[9,8,2],[4,5,6]]
+        expected = [[8,2,3],[9,6,7],[4,5,1]]
+        self.assertEqual(Solution().sortMatrix(grid), expected)
+    
+    def test_case_2(self):
+        grid = [[0,1],[1,2]]
+        expected = [[2,1],[1,0]]
+        self.assertEqual(Solution().sortMatrix(grid), expected)
+        
+    def test_case_3(self):
+        grid = [[1]]
+        expected = [[1]]
+        self.assertEqual(Solution().sortMatrix(grid), expected)
+
+if __name__ == "__main__":
+    unittest.main()
