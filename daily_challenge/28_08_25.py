398087722311b3a7e260992d04b8560cf3512c29
Joel <reyesvargasjoel@gmail.com>
2025-08-28 22:36:18 -0600

    feat: unit test added

--- daily_challenge/28_08_25.py
+++ daily_challenge/28_08_25.py
@@ -6,3 +6,25 @@ class Solution:
     def flowerGame(self, n: int, m: int) -> int:
         return (m * n) // 2
     
+# Unittests
+import unittest
+
+class TestSolution(unittest.TestCase):
+    
+    def test_case_1(self):
+        n, m = 2, 3
+        expected = 3
+        self.assertEqual(Solution().flowerGame(n, m), expected)
+    
+    def test_case_2(self):
+        n, m = 1, 1
+        expected = 0
+        self.assertEqual(Solution().flowerGame(n, m), expected)
+        
+    def test_case_3(self):
+        n, m = 4, 5
+        expected = 10
+        self.assertEqual(Solution().flowerGame(n, m), expected)
+    
+if __name__ == "__main__":
+    unittest.main()
