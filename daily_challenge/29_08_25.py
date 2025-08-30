832e202c89ea36f0c8e1113eb7f7f1df7dbaf270
Joel <reyesvargasjoel@gmail.com>
2025-08-29 20:19:23 -0600

    feat: problem solved

--- daily_challenge/29_08_25.py
+++ daily_challenge/29_08_25.py
@@ -1,3 +1,34 @@
 """
 https://leetcode.com/problems/valid-sudoku/description/?envType=daily-question&envId=2025-08-30
 """
+
+from typing import List
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for row in board:
+            seen = ''
+            for n in row:
+                if n in seen:
+                    return False
+                if n != '.':
+                    seen += n
+        for i in range(9):
+            seen = ''
+            for j in range(9):
+                n = board[j][i]
+                if n in seen:
+                    return False
+                if n != '.':
+                    seen += n
+        for a in range(3):
+            for b in range(3):
+                seen = ''
+                for i in range(3):
+                    for j in range(3):
+                        n = board[(3*a)+i][(3*b)+j]
+                        if n in seen:
+                            return False
+                        if n != '.':
+                            seen += n
+        return True
+    
