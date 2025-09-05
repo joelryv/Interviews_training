ac9cccfaec187aa823987c8fd6478651d66c24e4
Joel <reyesvargasjoel@gmail.com>
2025-09-05 00:00:56 -0600

    feat: problem solved

--- daily_challenge/09_2025/04_09_25.py
+++ daily_challenge/09_2025/04_09_25.py
@@ -2,3 +2,13 @@
 https://leetcode.com/problems/minimum-operations-to-make-the-integer-zero/description/?envType=daily-question&envId=2025-09-05
 """
 
+class Solution:
+    def makeTheIntegerZero(self, num1: int, num2: int) -> int:
+        k = 1
+        while True:
+            x = num1 - num2 * k
+            if x < k:
+                return -1
+            if k >= x.bit_count():
+                return k
+            k += 1
