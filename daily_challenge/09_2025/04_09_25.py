379b45e763f928af87a887acf11c8ae4e966f343
Joel <reyesvargasjoel@gmail.com>
2025-09-05 00:04:20 -0600

    feat: second solution added

--- daily_challenge/09_2025/04_09_25.py
+++ daily_challenge/09_2025/04_09_25.py
@@ -13,6 +13,22 @@ class Solution:
                 return k
             k += 1
 
+class Solution2:
+    def makeTheIntegerZero(self, num1: int, num2: int) -> int:
+        """
+        m
+        num1 = num2 * m + (x = 2^i + 2^(i') + ..) term -> m
+
+        num1 - num2 * m =  
+        """
+
+        for i in range(61):
+            target = num1 - num2 * i
+            if target >= 0 and target.bit_count() <= i <= target:
+                return i
+        
+        return -1
+
 # Unit tests
 import unittest
 
