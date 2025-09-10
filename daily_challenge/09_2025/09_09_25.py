923f9dbb8b186c77b45baa6fc7ebee9fe420d1b3
Joel <reyesvargasjoel@gmail.com>
2025-09-09 18:29:17 -0600

    feat: problem solved

--- daily_challenge/09_2025/09_09_25.py
+++ daily_challenge/09_2025/09_09_25.py
@@ -1,3 +1,23 @@
 """
 https://leetcode.com/problems/number-of-people-aware-of-a-secret/description/?envType=daily-question&envId=2025-09-09
 """
+
+class Solution:
+    def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
+        knows = [0] * n
+        knows[0] = 1
+        shared = 0
+        total = 1
+
+        for day in range(delay, forget):
+            shared += knows[day - delay]
+            total += shared
+            knows[day] = shared 
+
+        for day in range(forget, n):
+            shared += knows[day - delay] - knows[day - forget]
+            total += shared - knows[day - forget]
+            knows[day] = shared
+        
+        return (total%(10**9+7))
+    
