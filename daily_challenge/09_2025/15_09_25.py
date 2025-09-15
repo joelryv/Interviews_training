3db703ce3e1fac9268e826f6e32536a49783cacb
Joel <reyesvargasjoel@gmail.com>
2025-09-15 15:03:37 -0600

    feat: problem solved

--- daily_challenge/09_2025/15_09_25.py
+++ daily_challenge/09_2025/15_09_25.py
@@ -1,3 +1,18 @@
 """
 https://leetcode.com/problems/maximum-number-of-words-you-can-type/description/?envType=daily-question&envId=2025-09-15
 """
+
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        brkn = {}
+        for c in brokenLetters:
+            brkn[c] = 1
+        words = text.split()
+        valid = len(words)
+        for word in words:
+            for l in word:
+                if l in brkn.keys():
+                    valid -= 1
+                    break
+        return valid
+    
