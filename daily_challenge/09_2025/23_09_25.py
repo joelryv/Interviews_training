f8ca5354448d25b1049cda49db4989fab3094fad
Joel <reyesvargasjoel@gmail.com>
2025-09-22 20:58:41 -0600

    feat: problem solved

--- daily_challenge/09_2025/23_09_25.py
+++ daily_challenge/09_2025/23_09_25.py
@@ -1,3 +1,20 @@
 """
 https://leetcode.com/problems/compare-version-numbers/description/?envType=daily-question&envId=2025-09-23
 """
+
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        v1 = [int(x) for x in version1.split('.')]
+        v2 = [int(x) for x in version2.split('.')]
+        max_l = max([len(v1), len(v2)])
+        while len(v1) < max_l:
+            v1.append(0)
+        while len(v2) < max_l:
+            v2.append(0)
+
+        for n1, n2 in zip(v1, v2):
+            if n1 < n2:
+                return -1
+            if n1 > n2:
+                return 1
+        return 0
