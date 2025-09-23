43f43611436ff2815eadd18f8707cde989838007
Joel <reyesvargasjoel@gmail.com>
2025-09-22 21:10:21 -0600

    feat: problem solution

--- daily_challenge/09_2025/22_09_25.py
+++ daily_challenge/09_2025/22_09_25.py
@@ -1,3 +1,17 @@
 """
 https://leetcode.com/problems/count-elements-with-maximum-frequency/description/?envType=daily-question&envId=2025-09-22
 """
+from typing import List
+
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        freq = {}
+        for n in nums:
+            freq[n] = freq.get(n, 0) + 1
+        max_freq = max(freq.values())
+        total = 0
+        for value in freq.values():
+            if value == max_freq:
+                total += value
+
+        return total
