7ff72757bc683a9c00db69c6d05901ff7daebcd5
Joel <reyesvargasjoel@gmail.com>
2025-09-29 00:10:37 -0600

    feat: problem solved

--- daily_challenge/09_2025/28_09_25.py
+++ daily_challenge/09_2025/28_09_25.py
@@ -1,3 +1,14 @@
 """
 https://leetcode.com/problems/largest-perimeter-triangle/description/?envType=daily-question
 """
+
+from typing import List
+
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+        while len(nums) >= 3:
+            if (nums[-3]+nums[-2]) > nums[-1]:
+                return nums[-3] + nums[-2] + nums[-1]
+            nums = nums[:-1]
+        return 0
