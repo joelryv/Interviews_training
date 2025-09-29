9a65a597d3f903f379bc0c72af2747b06bcaa80c
Joel <reyesvargasjoel@gmail.com>
2025-09-29 00:11:38 -0600

    feat: unit tests added

--- daily_challenge/09_2025/28_09_25.py
+++ daily_challenge/09_2025/28_09_25.py
@@ -12,3 +12,19 @@ class Solution:
                 return nums[-3] + nums[-2] + nums[-1]
             nums = nums[:-1]
         return 0
+    
+# Unit tests
+import unittest
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_largestPerimeter(self):
+        self.assertEqual(self.solution.largestPerimeter([2,1,2]), 5)
+        self.assertEqual(self.solution.largestPerimeter([1,2,1]), 0)
+        self.assertEqual(self.solution.largestPerimeter([3,2,3,4]), 10)
+        self.assertEqual(self.solution.largestPerimeter([3,6,2,3]), 8)
+
+if __name__ == "__main__":
+    unittest.main()
