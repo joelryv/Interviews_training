6d811ee2d98397261039f9274389ad09f5450101
Joel <reyesvargasjoel@gmail.com>
2025-09-29 22:03:02 -0600

    feat: problem solved

--- daily_challenge/09_2025/30_09_25.py
+++ daily_challenge/09_2025/30_09_25.py
@@ -1,3 +1,14 @@
 """
 https://leetcode.com/problems/find-triangular-sum-of-an-array/?envType=daily-question&envId=2025-09-30
 """
+
+from typing import List
+
+class Solution:
+    def triangularSum(self, nums: List[int]) -> int:
+        n = len(nums)
+        while n > 1:
+            nums = [(nums[i] + nums[i+1])%10 for i in range(n-1)]
+            n -= 1
+        return nums[0]
+    
