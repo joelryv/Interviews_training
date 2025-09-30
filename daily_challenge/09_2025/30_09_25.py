b9c880b2d8fe28c538afb9dd9eaafff7f02c7bdf
Joel <reyesvargasjoel@gmail.com>
2025-09-29 22:04:32 -0600

    feat: unit test added

--- daily_challenge/09_2025/30_09_25.py
+++ daily_challenge/09_2025/30_09_25.py
@@ -12,3 +12,18 @@ class Solution:
             n -= 1
         return nums[0]
     
+# Unit tests
+import unittest
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_triangularSum(self):
+        self.assertEqual(self.solution.triangularSum([1,2,3,4,5]), 8)
+        self.assertEqual(self.solution.triangularSum([5]), 5)
+        self.assertEqual(self.solution.triangularSum([2,3,4,5,6,7]), 4)
+        self.assertEqual(self.solution.triangularSum([0,1,2,3,4,5,6,7,8,9]), 4)
+
+if __name__ == "__main__":
+    unittest.main()
