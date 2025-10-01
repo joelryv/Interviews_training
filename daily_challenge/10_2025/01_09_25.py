86ccc0b1d757c655909b5cfaae18505c85b0987f
Joel <reyesvargasjoel@gmail.com>
2025-09-30 21:57:22 -0600

    feat: problem solved

--- daily_challenge/10_2025/01_09_25.py
+++ daily_challenge/10_2025/01_09_25.py
@@ -1,3 +1,13 @@
 """
 https://leetcode.com/problems/water-bottles/description/?envType=daily-question
 """
+
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        total = numBottles
+        while True:
+            exchange = numBottles//numExchange
+            if exchange == 0:
+                return total
+            total += exchange
+
