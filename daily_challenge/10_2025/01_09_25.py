bd8e0c0a26ff8a39bdd94f66c5103fa9a28853e2
Joel <reyesvargasjoel@gmail.com>
2025-09-30 22:00:35 -0600

    feat: unit test added

--- daily_challenge/10_2025/01_09_25.py
+++ daily_challenge/10_2025/01_09_25.py
@@ -10,4 +10,22 @@ class Solution:
             if exchange == 0:
                 return total
             total += exchange
+            residuo = numBottles%numExchange
+            numBottles = exchange + residuo
 
+# Unit tests
+import unittest
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_numWaterBottles(self):
+        self.assertEqual(self.solution.numWaterBottles(9, 3), 13)
+        self.assertEqual(self.solution.numWaterBottles(15, 4), 19)
+        self.assertEqual(self.solution.numWaterBottles(5, 5), 6)
+        self.assertEqual(self.solution.numWaterBottles(2, 3), 2)
+        self.assertEqual(self.solution.numWaterBottles(6, 2), 11)
+
+if __name__ == "__main__":
+    unittest.main()
