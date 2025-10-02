db6d7ec5c4ee7361bc6eb8cafa6f282c900cafd3
Joel <reyesvargasjoel@gmail.com>
2025-10-01 23:59:11 -0600

    feat: problem solved

--- daily_challenge/10_2025/02_10_25.py
+++ daily_challenge/10_2025/02_10_25.py
@@ -1,3 +1,19 @@
 """
 https://leetcode.com/problems/water-bottles-ii/submissions/1788889507/?envType=daily-question&envId=2025-10-02
 """
+
+class Solution:
+    def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+        empty = 0
+        totalDrunk = 0
+        while numBottles > 0:
+            totalDrunk += numBottles
+            empty += numBottles
+            numBottles = 0
+            while empty >= numExchange:
+                empty -= numExchange
+                numExchange += 1
+                numBottles += 1
+        return totalDrunk
+    
+
