4a88906af6bbb461bb0455c78d368b9b3da7763d
Joel <reyesvargasjoel@gmail.com>
2025-10-02 00:00:26 -0600

    feat: unit test added

--- daily_challenge/10_2025/02_10_25.py
+++ daily_challenge/10_2025/02_10_25.py
@@ -17,3 +17,18 @@ class Solution:
         return totalDrunk
     
 
+# Unit tests
+import unittest
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_maxBottlesDrunk(self):
+        self.assertEqual(self.solution.maxBottlesDrunk(9, 3), 11)
+        self.assertEqual(self.solution.maxBottlesDrunk(15, 4), 18)
+        self.assertEqual(self.solution.maxBottlesDrunk(5, 5), 6)
+        self.assertEqual(self.solution.maxBottlesDrunk(2, 3), 2)
+        self.assertEqual(self.solution.maxBottlesDrunk(6, 2), 8)
+    
+if __name__ == "__main__":
+    unittest.main()
