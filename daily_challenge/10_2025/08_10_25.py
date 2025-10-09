73e7987d1abd8d59cf72678d09ccba396fdd4234
Joel <reyesvargasjoel@gmail.com>
2025-10-08 22:44:54 -0600

    feat: problem solved

--- daily_challenge/10_2025/08_10_25.py
+++ daily_challenge/10_2025/08_10_25.py
@@ -1,3 +1,15 @@
 """
 https://leetcode.com/problems/find-the-minimum-amount-of-time-to-brew-potions/description/?envType=daily-question&envId=2025-10-09
 """
+
+from typing import List
+class Solution:
+    def minTime(self, skill: List[int], mana: List[int]) -> int:
+        done = [0] * (len(skill) + 1)
+        for i in range(len(mana)):
+            for j in range(len(skill)):
+                done[j + 1] = max(done[j + 1], done[j]) + mana[i] * skill[j]
+            for k in range(len(skill)-1, 0, -1):
+                done[k] = done[k + 1] - mana[i] * skill[k]
+
+        return done[-1]
