a73d2453b55747c4e79b41439ed47f7a47dd7248
Joel <reyesvargasjoel@gmail.com>
2025-10-10 00:43:26 -0600

    feat: unit tests added

--- daily_challenge/10_2025/09_10_25.py
+++ daily_challenge/10_2025/09_10_25.py
@@ -9,3 +9,18 @@ class Solution:
         for i in range(len(energy) - k - 1, -1, -1):
             energy[i] += energy[i + k]
         return max(energy)
+    
+# Unit test
+import unittest
+class Test(unittest.TestCase):
+    def test(self):
+        s = Solution()
+        self.assertEqual(s.maximumEnergy([1,-2,0,9,-1,-2], 3), 10)
+        self.assertEqual(s.maximumEnergy([3,-1,-1,4,-1,5], 2), 9)
+        self.assertEqual(s.maximumEnergy([-5,-3,-3,-2,-1], 2), -1)
+        self.assertEqual(s.maximumEnergy([10,20,30], 1), 60)
+        self.assertEqual(s.maximumEnergy([100], 1), 100)
+        self.assertEqual(s.maximumEnergy([1,2,3,4,5,6], 3), 9)
+
+if __name__ == "__main__":
+    unittest.main()
