05ed24e6b07bf896713339d47d87609edc09fa71
Joel <reyesvargasjoel@gmail.com>
2025-10-10 00:42:17 -0600

    feat: problem solved

--- daily_challenge/10_2025/09_10_25.py
+++ daily_challenge/10_2025/09_10_25.py
@@ -1,3 +1,11 @@
 """"
 https://leetcode.com/problems/taking-maximum-energy-from-the-mystic-dungeon/description/?envType=daily-question&envId=2025-10-10
 """
+
+from typing import List
+
+class Solution:
+    def maximumEnergy(self, energy: List[int], k: int) -> int:
+        for i in range(len(energy) - k - 1, -1, -1):
+            energy[i] += energy[i + k]
+        return max(energy)
