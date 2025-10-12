61333bc49352a781891979f76bbe8a00da03c3f1
Joel <reyesvargasjoel@gmail.com>
2025-10-11 22:25:37 -0600

    feat: problem definition

---
+++ daily_challenge/10_2025/11_10_25.py
@@ -0,0 +1,5 @@
+"""
+https://leetcode.com/problems/find-sum-of-array-product-of-magical-sequences/solutions/7267995/beats-99-explained/?envType=daily-question&envId=2025-10-12
+"""
+
+from typing import List
