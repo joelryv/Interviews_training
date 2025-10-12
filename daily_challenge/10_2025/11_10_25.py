c995ba9e9f07847ef76c310d05b52a578ae8b676
Joel <reyesvargasjoel@gmail.com>
2025-10-11 22:31:30 -0600

    feat: problem solved

--- daily_challenge/10_2025/11_10_25.py
+++ daily_challenge/10_2025/11_10_25.py
@@ -2,4 +2,28 @@
 https://leetcode.com/problems/find-sum-of-array-product-of-magical-sequences/solutions/7267995/beats-99-explained/?envType=daily-question&envId=2025-10-12
 """
 
+MOD = 10**9 + 7
+from functools import lru_cache
+import math
 from typing import List
+
+class Solution:
+    def magicalSum(self, m: int, k: int, nums: List[int]) -> int:
+        @lru_cache(None)
+        def dfs(remaining, odd_needed, index, carry):
+            if remaining < 0 or odd_needed < 0 or remaining + carry.bit_count() < odd_needed:
+                return 0
+            if remaining == 0:
+                return 1 if odd_needed == carry.bit_count() else 0
+            if index >= len(nums):
+                return 0
+            
+            ans = 0
+            for take in range(remaining + 1):
+                ways = math.comb(remaining, take) * pow(nums[index], take, MOD) % MOD
+                new_carry = carry + take
+                ans += ways * dfs(remaining - take, odd_needed - (new_carry % 2), index + 1, new_carry // 2)
+                ans %= MOD
+            return ans
+        
+        return dfs(m, k, 0, 0)
