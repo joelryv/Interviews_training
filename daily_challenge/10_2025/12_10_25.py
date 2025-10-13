8dee37e8803023e5c922bc6f7ab29650643de227
Joel <reyesvargasjoel@gmail.com>
2025-10-12 23:52:54 -0600

    feat: problem solved

--- daily_challenge/10_2025/12_10_25.py
+++ daily_challenge/10_2025/12_10_25.py
@@ -1,3 +1,15 @@
 """
 https://leetcode.com/problems/find-resultant-array-after-removing-anagrams/description/?envType=daily-question&envId=2025-10-13
 """
+
+from typing import List
+
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        result = [words[0]]
+        for i in range(1, len(words)):
+            current = sorted(result[-1])
+            if sorted(words[i]) != current:
+                result.append(words[i])
+        return result
+    
