9901740d7513060b7b5ed45bc12d25388bda7fca
Joel <reyesvargasjoel@gmail.com>
2025-10-13 22:56:54 -0600

    feat: problem solved

--- daily_challenge/10_2025/13_10_25.py
+++ daily_challenge/10_2025/13_10_25.py
@@ -2,3 +2,19 @@
 https://leetcode.com/problems/adjacent-increasing-subarrays-detection-i/description/?envType=daily-question&envId=2025-10-14
 """
 
+from typing import List
+
+class Solution:
+    def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
+        prev = 0
+        current = 1
+        for i in range(1, len(nums)):
+            if nums[i-1] < nums[i]:
+                current += 1
+            else:
+                prev = current
+                current = 1
+            if (current >= k and prev >= k) or current >= (2*k):
+                return True
+        return False
+    
