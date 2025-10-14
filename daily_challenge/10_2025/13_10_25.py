17b4f7c80b28ba521fdea92491c784057cf7eda1
Joel <reyesvargasjoel@gmail.com>
2025-10-13 23:00:15 -0600

    feat: unit test added

--- daily_challenge/10_2025/13_10_25.py
+++ daily_challenge/10_2025/13_10_25.py
@@ -18,3 +18,22 @@ class Solution:
                 return True
         return False
     
+# Unittest
+import unittest 
+
+class TestSolution(unittest.TestCase):
+    def test_hasIncreasingSubarrays(self):
+        solution = Solution()
+        self.assertTrue(solution.hasIncreasingSubarrays([2,4,5,3,4,5,6], 3))
+        self.assertFalse(solution.hasIncreasingSubarrays([1,1,1,1], 2))
+        self.assertTrue(solution.hasIncreasingSubarrays([1,2,3,4,5], 2))
+        self.assertFalse(solution.hasIncreasingSubarrays([5,4,3,2,1], 2))
+        self.assertTrue(solution.hasIncreasingSubarrays([1,2,1,2,1,2], 2))
+        self.assertFalse(solution.hasIncreasingSubarrays([1,2,3], 2))
+        self.assertTrue(solution.hasIncreasingSubarrays([1,2], 1))
+        self.assertFalse(solution.hasIncreasingSubarrays([2,1], 2))
+        self.assertTrue(solution.hasIncreasingSubarrays([1,2,3,4], 2))
+        self.assertFalse(solution.hasIncreasingSubarrays([3,2,1], 2))
+
+if __name__ == "__main__":
+    unittest.main()
