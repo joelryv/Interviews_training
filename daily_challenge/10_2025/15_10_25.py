0c15f929dfd711abe3fcb5929adeefefd81175c6
Joel <reyesvargasjoel@gmail.com>
2025-10-15 22:05:42 -0600

    feat: unit tests added

--- daily_challenge/10_2025/15_10_25.py
+++ daily_challenge/10_2025/15_10_25.py
@@ -16,3 +16,45 @@ class Solution:
                 min_i = i
                 min_mod = mod
         return min_mod * value + min_i
+    
+
+# Unit tests
+import unittest
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_example1(self):
+        nums = [1, -10, 7, 13, 6, 8]
+        value = 5
+        expected = 4
+        self.assertEqual(self.solution.findSmallestInteger(nums, value), expected)
+
+    def test_example2(self):
+        nums = [0, 1, 2, 3]
+        value = 4
+        expected = 4
+        self.assertEqual(self.solution.findSmallestInteger(nums, value), expected)
+
+    def test_additional1(self):
+        nums = [5, 10, 15]
+        value = 5
+        expected = 1
+        self.assertEqual(self.solution.findSmallestInteger(nums, value), expected)
+
+    def test_additional2(self):
+        nums = [-1, -2, -3]
+        value = 4
+        expected = 0
+        self.assertEqual(self.solution.findSmallestInteger(nums, value), expected)
+
+    def test_additional3(self):
+        nums = [0, 0, 0]
+        value = 1
+        expected = 3
+        self.assertEqual(self.solution.findSmallestInteger(nums, value), expected)
+
+
+if __name__=='__main__':
+    unittest.main()
